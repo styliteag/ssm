3120ad334314eb7c2a300574bd9a1ad98031ecbf
jeidnx <git@domainhier.de>
2025-03-26 16:47:53 +0100

    feat: allow nesting modals

--- static/forms.js
+++ static/forms.js
@@ -1,53 +1,95 @@
 const form_response_dialog = document.querySelector("#form_response_dialog");
 const snackbar = document.querySelector("#snackbar");
+const modal_stack = document.getElementById("modal_stack");
+let lastXhr = "";
 
+/* Closes the current modal, and returns wether all Modals are closed */
 function closeModal() {
+  if (modal_stack.childElementCount > 0) {
+    while (form_response_dialog.firstChild) {
+      form_response_dialog.removeChild(form_response_dialog.firstChild);
+    }
+
+    const stackPop = modal_stack.lastChild;
+    while (stackPop.firstChild) {
+      form_response_dialog.appendChild(
+        stackPop.removeChild(stackPop.firstChild),
+      );
+    }
+    htmx.remove(stackPop);
+    return false;
+  }
+
   form_response_dialog.close();
+  return true;
 }
 
-function show_response(html, is_success) {
-  const div = document.createElement('div');
+/* Appends a toast to the snackbar list */
+function show_response_toast(html, is_success) {
+  const div = document.createElement("div");
   div.className = is_success ? "form_success" : "form_error";
 
-  const template = document.createElement('template');
+  const template = document.createElement("template");
   template.innerHTML = html;
   const node = template.content.cloneNode(true);
   div.appendChild(node);
 
   setTimeout(() => {
-    div.remove();
-  }, 5000)
+    htmx.remove(div);
+  }, 5000);
 
   snackbar.prepend(div);
 }
 
-document.body.addEventListener("htmx:afterRequest", (event) => {
-  const isFormResponse = (event.detail.xhr.getResponseHeader("X-FORM") === "true");
-  const isSuccess = (event.detail.successful === true);
+htmx.on("htmx:afterRequest", (event) => {
+  // Prevent event from triggering twice.
+  // This seems to be an htmx bug?
+  if (lastXhr === event.detail.xhr) return;
+  lastXhr = event.detail.xhr;
+
+  const isFormResponse =
+    event.detail.xhr.getResponseHeader("X-FORM") === "true";
+  const isSuccess = event.detail.successful === true;
   if (!isSuccess) {
     if (isFormResponse) {
-      show_response(event.detail.xhr.response, false);
+      show_response_toast(event.detail.xhr.response, false);
     } else {
       const statusCode = event.detail.xhr.status;
-      show_response(`<b>Request Failed</b><br><i>
-        ${(typeof statusCode === "number" && statusCode !== 0) ?
-          `Status code: <code>${statusCode}</code></i>` :
-          "No details"
-        }`)
+      show_response_toast(`<b>Request Failed</b><br><i>
+        ${
+          typeof statusCode === "number" && statusCode !== 0
+            ? `Status code: <code>${statusCode}</code></i>`
+            : "No details"
+        }`);
     }
     return;
   }
 
   if (!isFormResponse) return;
 
-  const openModal = (event.detail.xhr.getResponseHeader("X-MODAL") === "open");
-  if (openModal) {
-    form_response_dialog.innerHTML = event.detail.xhr.response;
-    htmx.process(form_response_dialog);
-    form_response_dialog.showModal();
-    return;
+  const doOpenModal = event.detail.xhr.getResponseHeader("X-MODAL") === "open";
+  if (!doOpenModal) {
+    if (closeModal()) {
+      show_response_toast(event.detail.xhr.response, true);
+    }
   }
-  closeModal();
+  const is_open = form_response_dialog.open;
 
-  show_response(event.detail.xhr.response, true);
+  if (is_open) {
+    const new_stack_entry = document.createElement("div");
+    while (form_response_dialog.firstChild) {
+      new_stack_entry.appendChild(
+        form_response_dialog.removeChild(form_response_dialog.firstChild),
+      );
+    }
+    modal_stack.appendChild(new_stack_entry);
+  }
+
+  htmx.swap(form_response_dialog, event.detail.xhr.response, {
+    swapStyle: "innerHTML",
+  });
+
+  if (!is_open) {
+    form_response_dialog.showModal();
+  }
 });
